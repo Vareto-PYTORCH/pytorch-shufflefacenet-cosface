add460e1e999e4bd4abc82573dedff1f925cdd34
Zayonix <68541043+Zayonix@users.noreply.github.com>
2020-09-14 21:33:07 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+BATCH_SIZE = 256
+SAVE_FREQ = 10
+TEST_FREQ = 5
+TOTAL_EPOCH = 70
+
+RESUME = ''
+SAVE_DIR = './model'
+MODEL_PRE = 'CASIA_ShuffleFaceNet_'
+
+
+CASIA_DATA_DIR = '/home/users/matheusb/recfaces/datasets/CASIA-WebFace/' #'/'CASIA-WebFace
+LFW_DATA_DIR = '/Users/saisi/Desktop/Deep Edge/Reveal Media - Face Recog/Cos_shuffle_pytorch/dataloader/lfwdata/'
+
+GPU = 0,1,2
